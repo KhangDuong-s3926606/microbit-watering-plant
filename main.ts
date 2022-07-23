e16a4874c95054b5dab21034c6d652b3e71f710d
KhangDuong-s3926606 <109841019+KhangDuong-s3926606@users.noreply.github.com>
2022-07-23 21:46:40 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,18 @@
+let MoistureReading = 0
 input.onButtonPressed(Button.A, function () {
     MoistureReading = Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, 0, 100)
     basic.showNumber(Math.round(MoistureReading))
     basic.clearScreen()
 })
-input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index <= 4; index++) {
-        led.plotBarGraph(
-        0,
-        5
-        )
-    }
-})
 input.onButtonPressed(Button.B, function () {
     MoistureReading = Math.map(pins.digitalReadPin(DigitalPin.P1), 0, 1023, 0, 100)
 })
-let MoistureReading = 0
-pins.digitalWritePin(DigitalPin.P8, 0)
-basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    basic.pause(2000)
-})
 basic.forever(function () {
     MoistureReading = Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, 0, 100)
-    if (MoistureReading < 1) {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(Math.round(MoistureReading))
+    }
+    if (MoistureReading < 30) {
         basic.showLeds(`
             # . . . #
             # . . . #
@@ -32,7 +20,7 @@ basic.forever(function () {
             # . . . #
             # # # # #
             `)
-    } else if (MoistureReading > 1 && MoistureReading <= 60) {
+    } else if (MoistureReading > 30 && MoistureReading <= 60) {
         basic.showLeds(`
             # . . . #
             # . . . #
