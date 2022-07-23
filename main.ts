6f28318f5ff0fbac6286d9f407a32bdd057f6198
KhangDuong-s3926606 <109841019+KhangDuong-s3926606@users.noreply.github.com>
2022-07-23 12:36:39 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,8 +20,6 @@ basic.forever(function () {
     basic.pause(2000)
 })
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P8, 0)
     MoistureReading = Math.map(pins.analogReadPin(AnalogPin.P0), 0, 1023, 0, 100)
     if (MoistureReading < 1) {
         basic.showLeds(`
@@ -31,8 +29,6 @@ basic.forever(function () {
             # . . . #
             # # # # #
             `)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P8, 0)
     } else if (MoistureReading > 1 && MoistureReading <= 60) {
         basic.showLeds(`
             # . . . #
@@ -41,8 +37,6 @@ basic.forever(function () {
             # # # # #
             # # # # #
             `)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P8, 0)
     } else {
         basic.showLeds(`
             # # # # #
@@ -51,7 +45,5 @@ basic.forever(function () {
             # # # # #
             # # # # #
             `)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P8, 0)
     }
 })
