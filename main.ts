fdd91cdb1e3c7f1e61e0ad00304611a0332045f7
KhangDuong-s3926606 <109841019+KhangDuong-s3926606@users.noreply.github.com>
2022-07-25 11:07:07 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,16 +26,16 @@ basic.showLeds(`
 basic.showLeds(`
     # # # # #
     # . . . .
-    # # # # #
+    # . . . .
     # . . . .
     # # # # #
     `)
 basic.showLeds(`
     # # # # #
-    # . . . #
-    # . . . #
-    # . . . #
-    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
     `)
 basic.pause(2000)
 basic.forever(function () {
